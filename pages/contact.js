afa59c52836ed60e47bcc60f65b12223bfc907b7
dondycles <johnrod.dondoyano@gmail.com>
2022-10-15 17:56:30 +0800

    modified:   pages/_app.js 	modified:   pages/contact.js 	modified:   styles/globals.css

--- pages/contact.js
+++ pages/contact.js
@@ -12,51 +12,29 @@ export default function Home() {
           Contact Us
         </title>
       </Head>
-
-      <div className='relative w-screen h-4/6 overflow-hidden shadow-customShadow'>
-        <Image className='w-screen h-auto object-center '  objectFit='cover' layout='fill' width={1980} height={1080} src={bgryImg} />
-      </div>
-      <div className='w-full h-2/6 flex flex-col'>
-        <div className=' flex gap-3 md:gap-5  justify-center items-center h-20 mt-0 mb-0 mx-auto text-md md:text-lg font-extrabold drop-shadow-[0px_3px_6px_rgba(0,0,0,0.5)]'>
+      <nav>
+      <div className=' flex gap-3 md:gap-5  justify-center items-center h-20 mt-0 mb-0 mx-auto text-md md:text-lg font-extrabold drop-shadow-[0px_3px_6px_rgba(0,0,0,0.5)]'>
           <div className='cursor-pointer text-gray-300 hover:text-white'>
             Home
           </div>
           <div className='cursor-pointer text-gray-300 hover:text-white'>
             Online Services
           </div>
-          <div className='cursor-pointer text-gray-300 hover:text-white'>
-            Contact Us
-          </div>
+
+          <Link href="/contact" passHref>
+            <a>
+              <div className='cursor-pointer text-gray-300 hover:text-white'>
+                Contact Us
+              </div>
+            </a>
+          </Link>
+
+          
           <div className='cursor-pointer text-gray-300 hover:text-white'>
             About
           </div>
         </div>
-
-        <div className='my-auto mx-auto w-full drop-shadow-[0px_3px_6px_rgba(0,0,0,0.5)]'>
-
-        <Link href="/signup" passHref>
-          <a>
-            <div className='mx-auto my-auto flex items-center justify-center gap-3 bg-accentColor rounded-full px-5 py-1 cursor-pointer origin-center font-medium hover:font-extrabold hover:px-[22px] transition-all ease-in-out text-gray-300 hover:text-white w-fit '>
-                Sign In
-            </div>
-          </a>
-        </Link>
-
-          <div className='mx-auto my-auto flex items-center justify-center gap-3 rounded-full px-5 py-1 font-thin transition-all ease-in-out text-gray-300 w-fit'>
-            or
-            <Link href="/login" >
-              <a>
-                <span className='text-accentColor cursor-pointer font-medium hover:font-extrabold transition-all ease-in-out'>Log In</span>
-              </a>
-            </Link>
-          </div>
-        </div>
-
-        <div className='mb-0 mx-auto font-thin opacity-[50%] text-[10px]'>
-          All Rights Reserved 2023
-        </div>
-
-      </div>
+      </nav>
     </div>
   )
 }
